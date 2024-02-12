8fd23e659afcedd74076ce5434bbb694ba72e195
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2024-02-11 21:34:34 -0600

    resolve

--- router/routerDispatcher.jsx
+++ router/routerDispatcher.jsx
@@ -1,19 +1,51 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthState } from "../utilities/firebaseUtils";
 import HomePage from "../src/components/homePage/homePage";
 import LoginPage from "../src/components/loginPage/loginPage";
 import ProfilePage from "../src/components/profilePage/profilePage";
 import RecommendationsPage from "../src/components/recommendationsPage/recommendationsPage";
 
 const RouteDispatcher = () => {
+  const [user, loading] = useAuthState();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  const PrivateRoute = ({ children }) => {
+    return user ? children : <Navigate replace to="/login" />;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate replace to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/recommendations" element={<RecommendationsPage />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <ProfilePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/recommendations"
+          element={
+            <PrivateRoute>
+              <RecommendationsPage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
